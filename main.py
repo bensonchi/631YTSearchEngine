b4a064fb25d1b6d0d287ed6625084223981242ef
baichuanchi <baichuan.chi@gmail.com>
2023-04-20 15:20:46 -0500

    Now UI successfully working with our Solr Server to retrieve video title as well as link to the actual video.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import tkinter as tk
 from tkinter import ttk
+import webbrowser
 import requests
 import json
+
 
 def search_videos():
     query = search_entry.get()
     if not query.strip():
-        result_text.set("Please enter a search query")
+        result_text.delete(1.0, tk.END)
+        result_text.insert(tk.END, "Please enter a search query")
         return
 
     solr_url = "http://localhost:8983/solr/tech_products/select"
@@ -22,15 +25,28 @@ def search_videos():
 
         results = data["response"]["docs"]
 
-        display_str = ""
-        for result in results:
-            result_dict = json.loads(result["_src_"])
-            display_str += result_dict["snippet"]["title"]
-            display_str += "\n"
-            display_str += "\n"
-        result_text.set(display_str)
+        result_text.delete(1.0, tk.END)
+        for r in results:
+            result_dict = json.loads(r["_src_"])
+            print(result_dict)
+            video_id = result_dict['id']['videoId']
+            r = result_dict["snippet"]
+            title = r['title']
+            url = f"https://www.youtube.com/watch?v={video_id}"
+
+            result_text.insert(tk.END, f"{title}\n", "title")
+            result_text.insert(tk.END, f"{url}\n", "url")
+    #             result_text.insert(tk.END, f"{url}\n", "url")
     except requests.RequestException as e:
-        result_text.set(f"An error occurred: {str(e)}")
+        result_text.delete(1.0, tk.END)
+        result_text.insert(tk.END, f"An error occurred: {str(e)}")
+
+
+def open_url(event):
+    start_index = result_text.index(f"@{event.x},{event.y} linestart")
+    end_index = result_text.index(f"@{event.x},{event.y} lineend")
+    url = result_text.get(start_index, end_index)
+    webbrowser.open(url)
 
 
 root = tk.Tk()
@@ -51,9 +67,13 @@ search_button.grid(column=2, row=0, sticky=tk.E)
 result_label = ttk.Label(main_frame, text="Results:")
 result_label.grid(column=0, row=1, sticky=(tk.W, tk.N))
 
-result_text = tk.StringVar()
-result_display = ttk.Label(main_frame, textvariable=result_text, anchor=tk.W, justify=tk.LEFT, wraplength=400)
-result_display.grid(column=1, row=1, columnspan=2, sticky=(tk.W, tk.E))
+result_text = tk.Text(main_frame, wrap=tk.WORD, width=50, height=15)
+result_text.grid(column=1, row=1, columnspan=2, sticky=(tk.W, tk.E))
+
+result_text.tag_configure("url", foreground="blue", underline=1)
+result_text.tag_bind("url", "<Enter>", lambda e: result_text.config(cursor="hand2"))
+result_text.tag_bind("url", "<Leave>", lambda e: result_text.config(cursor=""))
+result_text.tag_bind("url", "<Button-1>", open_url)
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
